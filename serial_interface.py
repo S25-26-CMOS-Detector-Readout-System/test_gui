b83c795937a3b912a82f0a88bd911890e1ff9c1e
Justin Winn <jwinn03@vt.edu>
2025-04-15 20:19:04 -0400

    Removed extraneous/redundant files

--- serial_interface.py
+++
@@ -1,8 +0,0 @@
-
-import socket
-
-# Create a UDP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# Bind to address and port - port 50007 chosen as it is unused by other applications
-# IP is static
-server_socket.bind(('192.168.137.6', 50007))
