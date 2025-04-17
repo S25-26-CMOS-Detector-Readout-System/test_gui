2b7b3e9ae424fdf29d9fdd7c0ea9a543c1bb2ce0
Justin Winn <jwinn03@vt.edu>
2025-04-17 16:19:32 -0400

    Improved exception handling

--- gui.py
+++ gui.py
@@ -56,8 +56,9 @@ DEFAULT_PORT = 50007
 
 SERIAL_COM_PORT = 'COM4' # Serial port name (Windows: 'COM1', Linux: '/dev/ttyUSB0', Mac: '/dev/tty.usbserial')
 
-IMAGE_DATA_LENGTH = 8
-            # just the data part, no header or leader - normally 6144, depends on row length
+
+IMAGE_DATA_LENGTH = 6144 # Just the data part, no header or leader - normally 6144, depends on row length
+            
 
 
 
@@ -98,12 +99,23 @@ class ConsoleRedirect:
         self.text_widget.config(state="normal")  
         self.text_widget.delete("1.0", tk.END)  
         self.text_widget.config(state="disabled") 
+        
 
     def write(self, message):
         self.text_widget.config(state="normal") 
+        # When print() is called, write() is called twice - once with the message, once with a newline
         self.text_widget.insert(tk.END, message) 
         self.text_widget.see(tk.END) 
         self.text_widget.config(state="disabled")  
+
+    """
+    def write(self, message, timestamp):
+        self.text_widget.config(state="normal") 
+        currTime = datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        self.text_widget.insert(tk.END, f"{currTime}: {message}\n")
+        self.text_widget.see(tk.END) 
+        self.text_widget.config(state="disabled")
+    """
 
     def flush(self):
         pass  # Required for compatibility with sys.stdout
@@ -112,7 +124,7 @@ class ConsoleRedirect:
 # implementation partially inspired by https://scribles.net/showing-video-image-on-tkinter-window-with-opencv/ 
 class MainWindow():
     def __init__(self, window):
-        self.log_to_file("GUI started")
+        
         self.window = window 
         self.window
         #self.cap = cap # image source
@@ -141,7 +153,6 @@ class MainWindow():
         # Main CMOS sensor output image
         self.mainImage = tk.Canvas(self.window, width=IMAGE_WIDTH/4, height=IMAGE_HEIGHT/4)
         self.mainImage.create_rectangle(0, 0, IMAGE_WIDTH/4, IMAGE_HEIGHT/4, fill="red", outline="")
-        #self.mainImage.pack(side=tk.TOP, padx=10, pady=10) 
         self.mainImage.grid(row=0, column=0, rowspan=6, columnspan=1)
         
         # Text box
@@ -152,48 +163,42 @@ class MainWindow():
 
         # Create buttons to call associated function
         self.button_1 = tk.Button(self.window, text="Reset", command=self.send_reset)
-        #self.button_1.pack(side=tk.LEFT, padx=10, pady=10)
         self.button_1.grid(row=0, column=1)
 
         self.button_2 = tk.Button(self.window, text="Abort", command=self.send_abort)
-        #self.button_2.pack(side=tk.LEFT, padx=10, pady=10)
         self.button_2.grid(row=1, column=1)
 
         self.button_3 = tk.Button(self.window, text="Enter Image Collection Mode", command=self.send_enter_image_collection)
-        #self.button_3.pack(side=tk.LEFT, padx=10, pady=10)
         self.button_3.grid(row=2, column=1)
 
         self.button_4 = tk.Button(self.window, text="Image Request", command=self.send_image_request)
-        #self.button_4.pack(side=tk.LEFT, padx=10, pady=10)
         self.button_4.grid(row=3, column=1)
 
         self.button_5 = tk.Button(self.window, text="Change sensor settings...", command=open_popup)
-        #self.button_5.pack(side=tk.LEFT, padx=10, pady=10)
         self.button_5.grid(row=4, column=1)
 
         self.button_6 = tk.Button(self.window, text="Toggle UDP/Serial", command=self.toggle_udp_serial)
-        #self.button_6.pack(side=tk.LEFT, padx=10, pady=10)
         self.button_6.grid(row=5, column=1)
 
         self.commModetext = tk.Text(self.window, width=15, height=1)
         self.commModetext.insert(1.0, "Using UDP")
         self.commModetext.config(state="disabled")
-        #self.commModetext.pack(side=tk.BOTTOM, padx=10, pady=10)
         self.commModetext.grid(row=6, column=1)
 
         self.tele = tk.Text(self.window, width=30, height=6)
         self.tele.insert(1.0, "Waiting for telemetry")
         self.tele.config(state="disabled")
-        #self.tele.pack(side=tk.BOTTOM, padx=10, pady=10)
         self.tele.grid(row=7, column=1)
 
         self.console_output = tk.Text(self.window, height=10, width=80, wrap="word")
         self.console_output.config(state="disabled")  
-        #self.console_output.pack(side=tk.BOTTOM, padx=10, pady=10)
         self.console_output.grid(row=7, column=0, padx=10)
 
         sys.stdout = ConsoleRedirect(self.console_output)  # Redirect stdout to the text box
         #sys.stderr = ConsoleRedirect(self.console_output)  # Redirect stderr to the text box
+        self.log_to_file("GUI started")
+
+        #self.window.rowconfigure(1, weight=1)  # Make the first column expandable
 
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
 
@@ -205,7 +210,7 @@ class MainWindow():
             self.data, self.addr = self.sock.recvfrom(4096)
             print(self.addr)
         except OSError as e:
-            self.log_to_file(f"Error creating UDP socket: {e}")
+            self.log_to_file(f"Error creating UDP socket (1): {e}")
 
         
         try: 
@@ -229,7 +234,7 @@ class MainWindow():
                 self.commModetext.pack(side=tk.BOTTOM, padx=10, pady=10)
                 
         except serial.SerialException as e:
-            print(f"Error opening serial port: {e}")
+            self.log_to_file(f"Error opening serial connection (1): {e}")
             self.ser = None
         
         self.frame_queue = queue.Queue() # Queue to hold frames for processing - maxsize prevents memory overflow
@@ -408,7 +413,7 @@ class MainWindow():
                 self.commModetext.config(state="disabled")
 
             except serial.SerialException as e:
-                print(f"Error opening serial port: {e}")
+                self.log_to_file(f"Error opening serial connection (2): {e}")
             
             
             
@@ -430,46 +435,62 @@ class MainWindow():
                 self.commModetext.config(state="disabled")
                 self.ser.close()
             except:
-                print("Error creating UDP socket")
+                self.log_to_file("Error creating UDP socket (2)")
             
 
     # Custom functions for each button
     def send_reset(self):
-        if (self.transmission_udp):
+        if (self.transmission_udp and self.sock != None):
             self.sock.sendto(int(CMD_RESET, 16).to_bytes(2), self.addr)
-        elif (self.ser.is_open):
+            self.log_to_file("Sent reset, UDP")
+        elif (not self.transmission_udp and self.ser.is_open):
             self.ser.write(str.encode(CMD_RESET))
+            self.log_to_file("Sent reset, serial")
+        else:
+            self.log_to_file("Failed to send reset - no connection established")
         
-        self.log_to_file("Sent reset")
+        
 
     def send_abort(self):
-        if (self.transmission_udp):
+        if (self.transmission_udp and self.sock != None):
             self.sock.sendto(int(CMD_ABORT, 16).to_bytes(2), self.addr)
-        elif (self.ser.is_open):
+            self.log_to_file("Sent abort, UDP")
+        elif (not self.transmission_udp and self.ser.is_open):
             self.ser.write(str.encode(CMD_ABORT))
+            self.log_to_file("Sent abort, serial")
+        else:
+            self.log_to_file("Failed to send abort - no connection established")
         
-        self.log_to_file("Sent abort")
 
     def send_enter_image_collection(self):
-        if (self.transmission_udp):
+        if (self.transmission_udp and self.sock != None):
             self.sock.sendto(int(CMD_P_IMAGE_ENABLE, 16).to_bytes(2), self.addr)
-            # add unix timecode: self.sock.sendto(int(CMD_P_IMAGE_ENABLE + self.get_unix_time_hex(), 16).to_bytes(10), self.addr)
-        elif (self.ser.is_open):
+            # add unix timecode: 
+            # self.sock.sendto(int(CMD_P_IMAGE_ENABLE + self.get_unix_time_hex(), 16).to_bytes(10), self.addr)
+            self.log_to_file("Sent enter image collection mode, UDP")
+        elif (not self.transmission_udp and self.ser.is_open):
             self.ser.write(str.encode(CMD_P_IMAGE_ENABLE) + str.encode(self.get_unix_time_hex()))
+            self.log_to_file("Sent enter image collection mode, serial")
+        else:
+            self.log_to_file("Failed to send enter image collection mode - no connection established")
         
-        self.log_to_file("Sent enter image collection mode")
 
     def send_image_request(self):
-        if (self.transmission_udp):
+        if (self.transmission_udp and self.sock != None):
             self.sock.sendto(int(CMD_IMAGE_REQUEST, 16).to_bytes(2), self.addr)
-        elif (self.ser.is_open):
+            self.log_to_file("Sent image request, UDP")
+        elif (not self.transmission_udp and self.ser.is_open):
             self.ser.write(str.encode(CMD_IMAGE_REQUEST))
+            self.log_to_file("Sent image request, serial")
+        else:
+            self.log_to_file("Failed to send image request - no connection established")
         
-        self.log_to_file("Sent image request")
+        
 
-    def change_sensor_settings(spec1): #spec1 instead of self?
+    def change_sensor_settings(self, spec1): #spec1 instead of self?
         # TODO
-        print("Sensor setting changed to " + str(spec1))
+        self.log_to_file("Sensor setting changed to " + str(spec1))
+
 
     # Get Unix timestamp as 64-bit integer to sent with packets
     def get_unix_time_hex():
@@ -486,12 +507,13 @@ class MainWindow():
         """
         # Open the CSV in append mode
         with open("packet_log.csv", mode="a", newline="") as file:
-            print(message)
+            
             writer = csv.writer(file)
             
             # Get the current timestamp
             timestamp = datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]  # Format: YYYY-MM-DD HH:MM:SS.mmm
-            
+            print(timestamp + ": " + message)
+
             # Write the log entry
             if (telemetry == None):
                 writer.writerow([timestamp, message])
@@ -499,8 +521,9 @@ class MainWindow():
                 writer.writerow([timestamp, message, telemetry.state, telemetry.temp1, telemetry.temp2, telemetry.voltage, telemetry.fault_code])
 
 
+
     def on_closing(self):
-        print("Closing GUI...")
+        self.log_to_file("Closing GUI")
         # Set a flag to stop the processing thread
         self.running = False
         # Close UDP socket if it exists
@@ -531,7 +554,7 @@ def open_popup():
         label_1.place(x=80,y=80)
         inputtxt = tk.Text(top, height = 1, width = 14, bg = "light yellow")
         inputtxt.pack(side=tk.LEFT, padx=10, pady=10)
-        button_1 = tk.Button(top, text="Change sensor settings...", command=lambda: change_sensor_settings(inputtxt.get("1.0", "end-1c")))
+        button_1 = tk.Button(top, text="Change sensor settings...", command=lambda: change_sensor_settings(inputtxt.get("1.0", "end-1c"), args = self))
         #button_6 = Button(top, text="Change sensor settings...", command=change_sensor_settings(100))
         button_1.pack(side=tk.LEFT, padx=10, pady=10)
 
@@ -581,7 +604,7 @@ def create_gui():
 
 if __name__ == "__main__":
     # Create and launch the GUI
-    # TODO: replace webcam placeholder with the latest frame from readout system in OpenCV format
+    
     print("Starting GUI - this may take a moment")
     create_gui()
     
